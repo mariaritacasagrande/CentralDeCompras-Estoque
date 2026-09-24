34f7bba2dc4e85feae364e76ca36e0a472e28712
Maria Rita Casagrande <mariarita@riseup.net>
2026-09-24 19:14:46 -0300

    Rota Ajustada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { PainelEstoque } from './pages/painel-estoque/painel-estoque';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: PainelEstoque },
+  { path: '**', redirectTo: '' },
+];
