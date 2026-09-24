d357a382229cfdf5fd0fe21fc67f7a21f0297e76
Maria Rita Casagrande <mariarita@riseup.net>
2026-09-24 18:27:46 -0300

    Create produto.model.ts

---
+++ src/app/models/produto.model.ts
@@ -0,0 +1,14 @@
+export interface Produto {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating?: { rate: number; count: number };
+}
+
+export interface ItemEstoque {
+  produto: Produto;
+  quantidade: number;
+}
